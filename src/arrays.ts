7e91d3226b710d3c7f6fde1655c32f812d3e9974
Kevin Kramer <kevinjkramer168@gmail.com>
2024-02-14 12:18:26 -0500

    Modified tripleNumbers function

--- src/arrays.ts
+++ src/arrays.ts
@@ -22,7 +22,11 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const copiedArray: number[] = numbers.map(
+        (originalNum: number): number => originalNum * 3
+    );
+
+    return copiedArray;
 }
 
 /**
