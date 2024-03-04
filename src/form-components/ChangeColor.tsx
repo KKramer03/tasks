efa1b675afed43082d6e4ca54818cbdf37f3b4fc
KKramer03 <kevinjkramer168@gmail.com>
2024-03-04 16:35:16 -0500

    More changes to ChangeColor function

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,8 +1,45 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const Colors = [
+    "red",
+    "blue",
+    "purple",
+    "magenta",
+    "pink",
+    "orange",
+    "black",
+    "white"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>(Colors[0]);
+    const colorCodes: Record<string, string> = {
+        red: "#FF0000",
+        blue: "#00FFFF"
+    };
+
+    const colorsList = Colors.map((currentColor: string) => (
+        <Form.Check
+            inline
+            key={currentColor}
+            type="radio"
+            name="colors"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setColor(e.target.value)
+            }
+            label={<p style={{ backgroundColor: { color } }}>{currentColor}</p>}
+            value={currentColor}
+            checked={color === currentColor}
+        />
+    ));
+
     return (
         <div>
+            {colorsList}
+            <p>
+                You have chosen <p>{color}</p>
+            </p>
             <h3>Change Color</h3>
         </div>
     );
